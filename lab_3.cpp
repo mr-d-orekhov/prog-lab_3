ba8f0410c99a7765657826a5c08d7ddd5ba7cb1e
Exi <mr.d.orekhov@gmail.com>
2024-03-16 09:47:30 +0300

    task_1

--- lab_3.cpp
+++ lab_3.cpp
@@ -1,20 +1,61 @@
 ﻿// lab_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define _USE_MATH_DEFINES
 
 #include <iostream>
 
+double round_to(double x, int n = 2) {
+	return round(x * pow(10.0, n)) / pow(10.0, n);
+}
+
+/*
+* Вывести на экран таблицу значений функции синус в диапазоне от − 2π до 2π с
+* шагом π/8.
+*/
+void task_1() {
+	float step = M_PI / 8;
+	float range_start = -2 * M_PI;
+	float range_end = 2 * M_PI;
+	int precision = 4;
+
+	std::cout << "Таблица значений с использованием цикла for:" << std::endl;
+
+	for (double i = range_start; i <= range_end; i += step) {
+		std::cout << round_to(i, precision) << " " << round_to(sin(i), precision) << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	std::cout << "Таблица значений с использованием цикла while:" << std::endl;
+
+	double j = range_start;
+	while (j <= range_end)
+	{
+		std::cout << round_to(j, precision) << " " << round_to(sin(j), precision) << std::endl;
+		j += step;
+	}
+
+	std::cout << std::endl;
+
+	std::cout << "Таблица значений с использованием цикла do while:" << std::endl;
+
+	double k = range_start;
+	do {
+		std::cout << round_to(k, precision) << " " << round_to(sin(k), precision) << std::endl;
+		k += step;
+	} while (k <= range_end);
+
+	std::cout << std::endl;
+}
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "");
+
+	task_1();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
